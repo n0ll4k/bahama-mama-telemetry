5c59ccd3ae1d167d16262ab9efc369d59ab6620b
Frederik Krämer <Frederik.Kraemer@ndt-global.com>
2023-02-15 10:07:49 +0100

    Added objects for different data types.

--- code/py/bmt_formats.py
+++ code/py/bmt_formats.py
@@ -15,3 +15,114 @@ TIMESTAMP_FMT = '<L'
 TRAVEL_INFORMATION_FMT = '<HH'
 # [0]: uint16   - Fork ADC data
 # [1]: uint16   - Shock ADC data
+
+class BmtSensorCalibration:
+    def __init__( self ):
+        self._sensor_id = 0
+        self._sensor_name = ""
+        self._adc_value_zero = 0
+        self._adc_value_max = 0
+        self._range_mm = 0
+    
+    def set_sensor_id( self, sensor_id ):
+        self._sensor_id = sensor_id
+    
+    def set_sensor_name( self, sensor_name ):
+        self._sensor_name = sensor_name
+    
+    def set_adc_value_zero( self, adc_val_zero ):
+        self._adc_value_zero = adc_val_zero
+
+    def set_adc_value_zero( self, adc_val_max ):
+        self._adc_value_max = adc_val_max
+
+    def set_range_mm( self, range_mm ):
+        self._range_mm = range_mm
+
+    def sensor_id( self ):
+        return self._sensor_id
+
+    def sensor_name( self ):
+        return self._sensor_name
+
+    def adc_value_zero( self ):
+        return self._adc_value_zero
+
+    def adc_value_max( self ):
+        return self._adc_value_max
+
+    def range_mm( self ):
+        return self._range_mm
+    
+class BmtBike:
+    def __init__( self ):
+        self._bike_id = 0
+        self._bike_name = ""
+        self._travel_fork_mm = 0
+        self._travel_rear_mm = 0
+        self._head_angle = 0.0
+        self._frame_linkage = list()
+    
+    def set_bike_id( self, bike_id ):
+        self._bike_id = bike_id
+
+    def set_bike_name( self, bike_name ):
+        self.set_bike_name = bike_name
+    
+    def set_travel_fork_mm( self, travel_fork_mm ):
+        self._travel_fork_mm = travel_fork_mm
+
+    def set_travel_rear_mm( self, travel_rear_mm ):
+        self._travel_read_mm = travel_rear_mm
+    
+    def set_head_angle( self, head_angle ):
+        self._head_angle = head_angle
+    
+    def set_framlinkage( self, linkage_data ):
+        self.set_framlinkage = linkage_data
+
+    def bike_id( self ):
+        return self._bike_id
+    
+    def bike_name( self ):
+        return self._bike_name
+
+    def travel_fork_mm( self ):
+        return self._travel_fork_mm
+    
+    def travel_rear_mm( self ):
+        return self._travel_rear_mm
+    
+    def head_angle( self ):
+        return self._head_angle
+    
+    def frame_linkage( self ):
+        return self._frame_linkage
+
+class BmtSetup:
+    def __init__( self ):
+        self._setup_id = 0
+        self._fork_sensor = BmtSensorCalibration()
+        self._shock_sensor = BmtSensorCalibration()
+        self._bike = BmtBike()
+    
+    def set_fork_sensor( self, fork_sensor ):
+        self._fork_sensor = fork_sensor
+    
+    def set_shock_sensor( self, shock_sensor ):
+        self._shock_sensor = shock_sensor
+
+    def set_bike( self, bike ):
+        self._bike = bike
+
+    def fork_sensor( self ):
+        return self._fork_sensor
+    
+    def shock_sensor( self ):
+        return self._shock_sensor
+    
+    def bike( self ):
+        return self._bike
+
+    
+
