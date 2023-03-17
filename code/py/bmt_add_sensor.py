7ce1cf6a82243e1ce5ad5ac19979bdbfddf98400
Frederik Kraemer <frederik.kraemer@gmail.com>
2023-03-17 19:44:45 +0100

    Added new error message box.

--- code/py/bmt_add_sensor.py
+++ code/py/bmt_add_sensor.py
@@ -1,5 +1,5 @@
 # QWidgets Container Objects
-from PyQt5.QtWidgets import QWidget, QFileDialog, QErrorMessage
+from PyQt5.QtWidgets import QWidget, QFileDialog, QMessageBox
 # PyQt uic
 from PyQt5 import uic
 # JSON
@@ -26,28 +26,22 @@ class AddSensorUi(QWidget):
         sensor = BmtSensorCalibration()
         sensor.set_sensor_name(self.sensor_name.text())
         if sensor.sensor_name() == "":
-            error = "Please set a name."
-            print( error )
-            error_dialog = QErrorMessage()
-            error_dialog.showMessage(error)
+            self.show_error("Please set a name.")
             return
         sensor.set_adc_value_zero(self.adcMin_val.value())
         sensor.set_adc_value_max(self.adcMax_val.value())
         if ( sensor.adc_value_max() == sensor.adc_value_zero()):
-            error = "Please set different min and max values."
-            print( error )
-            error_dialog = QErrorMessage()
-            error_dialog.showMessage(error)
+            self.show_error("Please set different min and max values.")
             return
         sensor.set_range_mm( self.range_val.value() )
         if sensor.range_mm() <= 0:
-            error = "Please set a sensor range larger 0mm."
-            print( error )
-            error_dialog = QErrorMessage()
-            error_dialog.showMessage(error)
+            self.show_error("Please set a sensor range larger 0mm.")
             return
         sensor.set_flip_travel( self.flip_travel_check.isChecked())
-        self.db.add_sensor( sensor)
+        return_value = self.db.add_sensor( sensor)
+        if return_value[0] == -1:
+            self.show_error( return_value[1] )
+            return
         self.close()
         
 
@@ -56,22 +50,17 @@ class AddSensorUi(QWidget):
         filename = QFileDialog.getOpenFileName(self,
                                                "Open Sensor JSON File", 
                                                "JSON Files (*.json)")
-        print(filename)
         sensor_data = self.load_json_file( filename[0])
-        if sensor_data == None:
-            error_dialog = QErrorMessage()
-            error_dialog.showMessage(f"Please select a correct json file.")
-        else:
-            try:
-                print( sensor_data )
+        if sensor_data is not None:
+            try:    
                 self.sensor_name.setText(sensor_data['sensor_name'])
                 self.adcMin_val.setValue(sensor_data['adc_val_zero'])
                 self.adcMax_val.setValue(sensor_data['adc_val_max'])
                 self.range_val.setValue(sensor_data['range_mm'])
                 self.flip_travel_check.setChecked(sensor_data['flip_travel'])
             except KeyError as errk:
-                error_dialog = QErrorMessage()
-                error_dialog.showMessage(f"Please select a correct json file.\n{errk}")
+                self.show_error(f"Please select a correct json file.\n{errk}" )
+
 
     def load_json_file(self, filename):
         try:
@@ -80,11 +69,17 @@ class AddSensorUi(QWidget):
             file.close()
             return json_data
         except json.decoder.JSONDecodeError as errj:
-            error_dialog = QErrorMessage()
-            error_dialog.showMessage(f"Please select a correct json file.\n{errj}")
+            self.show_error(f"Please select a correct json file.\n{errj}" )
         except FileNotFoundError as errf:
-            error_dialog = QErrorMessage()
-            error_dialog.showMessage(f"Please select a correct json file.\n{errf}")
+            self.show_error(f"Please select a correct json file.\n{errf}" )
+
+    def show_error( self, message ):
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Critical)
+        msg.setText("Error")
+        msg.setInformativeText(message)
+        msg.setWindowTitle("Error")
+        msg.exec_()
 
 
         
