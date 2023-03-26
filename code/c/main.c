5439f16b3629c1806f76ba9beb428c9ef1bdba86
Frederik Kraemer <frederik.kraemer@gmail.com>
2023-03-26 13:39:27 +0200

    Added WiFi AP functionality. Button debounce with polling timer instead of IRQ seems to fix the problem. TODO: DHCP/DNS/TCP-Server.

--- code/c/main.c
+++ code/c/main.c
@@ -21,8 +21,9 @@
 #include "adc_collector/adc_collector.h"
 #include "sd_if/sd_if.h"
 //---------------------------------------------------------------------------
-#define BTN_START_STOP      21
-#define BTN_WIFI            20
+#define BTN_START_STOP      1
+#define BTN_WIFI            5
+#define INPUT_LEN           2
 #define STATE_LED_PIN       22
 //---------------------------------------------------------------------------
 enum app_state_e {
@@ -33,7 +34,8 @@ enum app_state_e {
     state_meas,
     state_stop,
     state_stop_file,
-    state_transfer 
+    state_transfer,
+    state_stop_transfer 
 };
 
 queue_t core_queue;
@@ -41,34 +43,46 @@ static const uint8_t debounce_time = 50;
 static bool buttton_pressed = false;
 static int32_t alarm_id = 0;
 enum app_state_e app_state = state_init;
+uint32_t input_array[INPUT_LEN] = {BTN_START_STOP, BTN_WIFI};
+static bool input_states[INPUT_LEN] = {0, 0};
+static bool led_blink_state = false;
+const char *ap_name = "BahamaMamaTelemetry";
+const char *password = "GetDrunkOnBMT";
 //---------------------------------------------------------------------------
 void sd_handling_core1( void );
 void bttn_init( void );
-void bttn_cb( uint gpio_no, uint32_t events );
-int64_t bttn_enable( alarm_id_t alarm_id, void * user_data );
+bool rep_timer_callback( struct repeating_timer *timer);
+bool debounce_start_bttn(void);
+bool debounce_wifi_bttn(void);
+void set_led_blink_state(bool state);
 //---------------------------------------------------------------------------
 
 /* Core0 Handling. */
-int main() {
+int main() 
+{
+    struct repeating_timer timer;
+
     stdio_init_all();
+
+    if (cyw43_arch_init()) {
+        printf("failed to initialise\n");
+        return 1;
+    }
+    cyw43_arch_enable_ap_mode(ap_name, password, CYW43_AUTH_WPA2_AES_PSK);
 
     gpio_init(STATE_LED_PIN);
     gpio_set_dir( STATE_LED_PIN, GPIO_OUT );
-    
+
     bttn_init();
+    add_repeating_timer_ms(-25, rep_timer_callback, NULL, &timer);
 
     queue_init( &core_queue, sizeof(queue_element_t), 1024 );
-  
+
     multicore_launch_core1(sd_handling_core1);
 
-    sleep_ms(1000);
-    /* TODO: Why is Wifi Chip Init crashing? */
-    /* cyw43_arch_init();
-    printf( "Return Code CYW43: %d\n", ret ); */
-  
     /* Initialize data collection. */
     if ( 0 != data_collector_init( &core_queue ) ) {
-        return -1;
+        return 1;
     }
 
     app_state = state_idle;
@@ -88,14 +102,20 @@ int main() {
                 gpio_put(STATE_LED_PIN, 1);
                 app_state = state_meas;
                 break;
-            case state_idle:
             case state_transfer:
+                /* TODO: Run TCP Server and transfer data. */
+                sleep_ms(25);
+                break;
+            case state_stop_transfer:
+                set_led_blink_state( false );
+                app_state = state_idle;
+                break;
+            case state_idle:
             default:
                 gpio_put(STATE_LED_PIN, 0);
                 sleep_ms(25);
                 break;
         }
-        
     }
 }
 
@@ -107,6 +127,7 @@ void sd_handling_core1(void)
     
     storage = sd_if_init();
     if ( NULL == storage ){
+        printf("failed to initialize sd card.");
         return;
     }
 
@@ -133,62 +154,93 @@ void sd_handling_core1(void)
         }
     }
 
-    /* We will not hit this. */
+    /* We should never hit this. */
     sd_if_deinit( storage );
 }
 
 //---------------------------------------------------------------------------
-void bttn_cb( uint gpio_no, uint32_t events )
+bool rep_timer_callback( struct repeating_timer *timer)
 {   
-    if ( buttton_pressed ) {
-        cancel_alarm( alarm_id );
-    } else {
-        buttton_pressed = true;
-        switch( gpio_no ) {
-            case BTN_START_STOP:
-                switch( app_state ) {
-                    case state_idle:
-                        app_state = state_start_file;
-                        break;
-                    case state_meas:
-                        app_state = state_stop;
-                        break;
-                    default:
-                        break;
-                }
+    static bool led_state = false;
+    static uint32_t led_cnt = 20;
+    if(debounce_start_bttn() ){
+        switch( app_state ) {
+            case state_idle:
+                app_state = state_start_file;
                 break;
-            case BTN_WIFI:
-                switch( app_state ) {
-                    case state_idle:
-                        app_state = state_transfer;
-                        break;
-                    case state_transfer:
-                        app_state = state_idle;
-                    default:
-                        break;
-                }
+            case state_meas:
+                app_state = state_stop;
                 break;
             default:
                 break;
         }
     }
 
-    alarm_id = add_alarm_in_ms( debounce_time, bttn_enable, NULL, false );
-}
+    if( debounce_wifi_bttn() ) {
+        switch( app_state ) {
+            case state_idle:
+                set_led_blink_state( true );
+                app_state = state_transfer;
+                break;
+            case state_transfer:
+                app_state = state_stop_transfer;
+            default:
+                break;
+        }
+    }
 
-int64_t bttn_enable( alarm_id_t alarm_id, void * user_data )
-{
-    buttton_pressed = false;
-    return 0;
+    if ( led_blink_state ) {
+        led_cnt--;
+        if (led_cnt == 0) {
+            led_cnt = 20;
+            if ( gpio_get( STATE_LED_PIN ) ){
+                gpio_put( STATE_LED_PIN, 0);
+            } else {
+                gpio_put( STATE_LED_PIN, 1);
+            }
+        }
+    }
+
+    return true;
 }
 
 void bttn_init( void )
 {
     gpio_init(BTN_START_STOP);
-    gpio_pull_down(BTN_START_STOP);
+    gpio_set_dir( BTN_START_STOP, GPIO_IN );
+    gpio_pull_up(BTN_START_STOP);
+
     gpio_init(BTN_WIFI);
-    gpio_pull_down(BTN_WIFI);
-    
-    gpio_set_irq_enabled_with_callback(BTN_START_STOP, GPIO_IRQ_LEVEL_HIGH, true, &bttn_cb);
-    gpio_set_irq_enabled(BTN_WIFI, GPIO_IRQ_LEVEL_HIGH, true);
+    gpio_set_dir( BTN_WIFI, GPIO_IN) ;
+    gpio_pull_up(BTN_WIFI);
+}
+
+bool debounce_start_bttn(void)
+{
+    static uint16_t bttn_state = 0;
+    bttn_state = ( bttn_state << 1 ) | !gpio_get(BTN_START_STOP) | 0xe000;
+    if ( bttn_state == 0xf000 ){
+        return true;
+    }
+    return false;
+}
+
+bool debounce_wifi_bttn(void)
+{
+    static uint16_t bttn_state = 0;
+    bttn_state = ( bttn_state << 1 ) | !gpio_get(BTN_WIFI) | 0xe000;
+    if ( bttn_state == 0xf000 ){
+        return true;
+    }
+    return false;
+}
+
+void set_led_blink_state(bool state)
+{
+    if ( state ) {
+        led_blink_state = state;
+    } else {
+        led_blink_state = state;
+        gpio_put( STATE_LED_PIN, 0);
+    }
 }
